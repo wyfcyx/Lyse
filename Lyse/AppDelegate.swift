c8c7868fd6ffa25160c9331234b522b4e8d805d5
wyfcyx <wyfcyx@gmail.com>
2016-03-21 19:42:16 +0800

    Initial Commit

---
+++ Lyse/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Lyse
+//
+//  Created by wyfcyx on 16/3/21.
+//  Copyright © 2016年 jz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
