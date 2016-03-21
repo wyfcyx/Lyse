bf037aba038415ba37d6b6b7d544dc5698089363
fortissimoX <fortissimoX@gmail.com>
2016-03-21 20:38:08 +0800

    Basic Update

--- Lyse/AppDelegate.swift
+++ Lyse/AppDelegate.swift
@@ -10,14 +10,17 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+	
+	var mainWindowController: MainWindowController?
 
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-    }
-
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		let mainWindowController = MainWindowController()
+		
+		mainWindowController.showWindow(self)
+		
+		self.mainWindowController = mainWindowController
+	}
+	
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
---
+++ Lyse/MainWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  MainWindowController.swift
+//  Lyse
+//
+//  Created by 石榴 on 3/21/16.
+//  Copyright © 2016 jz. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+	override var windowNibName: String {
+		return "MainWindowController"
+	}
+	
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
